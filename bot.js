0840ad09f57a2d4ed628ac9c91e919c8870c858f
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-12-02 15:29:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,20 +18,4 @@ if(mohamed) {
 setTimeout(() => mohamed.send(`__ WēŁçØmĘ Ťö ** Last Night ** ŞėŘvËŕ... __`), 4000)        
 }
 });
-
-client.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => { //code bot not leave room voice //Bot Is Online
-    client.channels.get("515595619826663424").join(); //by : Toxic Codes
-    });
-
 client.login(process.env.BOT_TOKEN); 
